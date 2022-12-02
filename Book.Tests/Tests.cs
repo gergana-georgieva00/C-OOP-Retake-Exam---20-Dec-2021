a496a143aebe1334d718997fb2b39f377ceb30eb
Gergana Georgieva <gerganagergana1@gmail.com>
2022-12-03 01:58:03 +0200

    C# OOP Retake Exam - 20 Dec 2021

--- Book.Tests/Tests.cs
+++ Book.Tests/Tests.cs
@@ -6,5 +6,18 @@
 
     public class Tests
     {
+        private Book book;
+
+        [SetUp]
+        public void SetUp()
+        {
+            book = new Book("bookName", "author");
+        }
+
+        [Test]
+        public void InvalidNameShouldThrow()
+        {
+            Assert.Throws<ArgumentException>(() => book = new Book("", "author"));
+        }
     }
 }
